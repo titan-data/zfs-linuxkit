068a73aabfd3480f13c6929735c2490c33606363
Avi Deitcher <avi@deitcher.net>
2021-03-23 20:44:42 +0200

    ensure we call --load when building docker image

--- build.sh
+++ build.sh
@@ -26,7 +26,8 @@ while read -r line; do
   fi
   VERSION_TAG=$(echo $KERNEL_IMAGE | tr '/' '-' | tr ':' '-')
   echo "Building openzfs $ZFS_VERSION for $KERNEL_IMAGE with alpine $ALPINE_VERSION"
-  docker build --build-arg ZFS_VERSION=$ZFS_VERSION --build-arg KERNEL_IMAGE=$KERNEL_IMAGE --build-arg ALPINE_VERSION=$ALPINE_VERSION  -t openzfs:install-${VERSION_TAG} .
+  docker build --build-arg ZFS_VERSION=$ZFS_VERSION --build-arg KERNEL_IMAGE=$KERNEL_IMAGE --build-arg ALPINE_VERSION=$ALPINE_VERSION  -t openzfs:install-${VERSION_TAG} --load .
+  echo "Built image, saving"
   docker save openzfs:install-${VERSION_TAG} > cache/zfs-${VERSION_TAG}.tar
   echo "Creating linuxkit images for $KERNEL_IMAGE"
   for i in linuxkit-*.tmpl.yml; do
