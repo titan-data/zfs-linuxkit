50c151b864a141ffb87147e04823dc5a006aa910
Avi Deitcher <avi@deitcher.net>
2021-03-24 12:59:59 +0200

    easier end reporting, and truncate long hashes

--- build.sh
+++ build.sh
@@ -8,6 +8,24 @@ target_image="$1"
 
 mkdir -p cache
 
+# determine our platform
+platform=$(uname -s)
+platform=$(echo ${platform} | tr '[:upper:]' '[:lower:]')
+hypervisor=""
+case ${platform} in
+linux)
+  hypervisor=qemu
+  ;;
+darwin)
+  hypervisor=hyperkit
+  ;;
+*)
+  echo "Unknown platform ${platform}"
+  exit 1
+esac
+
+outlines=""
+
 # read the versions file to see what we support
 while read -r line; do
   # ignore lines that start with #
@@ -24,6 +42,7 @@ while read -r line; do
   if [ "$target_image" != "$KERNEL_IMAGE" -a "$target_image" != "all" ]; then
     continue
   fi
+  outlines="${outlines}\n${KERNEL_IMAGE} ready for test, run:"
   VERSION_TAG=$(echo $KERNEL_IMAGE | tr '/' '-' | tr ':' '-')
   echo "Building openzfs $ZFS_VERSION for $KERNEL_IMAGE with alpine $ALPINE_VERSION"
   docker build --build-arg ZFS_VERSION=$ZFS_VERSION --build-arg KERNEL_IMAGE=$KERNEL_IMAGE --build-arg ALPINE_VERSION=$ALPINE_VERSION  -t openzfs:install-${VERSION_TAG} --load .
@@ -32,16 +51,29 @@ while read -r line; do
   echo "Creating linuxkit images for $KERNEL_IMAGE"
   for i in linuxkit-*.tmpl.yml; do
     outfile=$(echo $i | sed 's/.tmpl\.yml//g')
-    outfile=cache/${outfile}-${VERSION_TAG}.yml
+    outfile=cache/${outfile}-${VERSION_TAG}
+    outfilesize=${#outfile}
+    if [ $outfilesize -gt 90 ]; then
+      if echo "$outfile" | grep -E '([a-f0-9]){40}$'; then
+        outfile=$(echo $outfile | sed 's/.\{33\}$//')
+      else
+         echo "outfile name would be ${outfile}, which is longer than the maximum 90 characters, cannot proceed"
+         exit 1
+      fi
+    fi
+
+    # check if the outfile is too long
+    # the maximum allowed in a socket file is 108, see https://man7.org/linux/man-pages/man7/unix.7.html
+    # and hyperkit adds another 18, so the max is 90. If it is longer than that, try to truncate a hash, if we can
+
+    outfile=${outfile}.yml
     cat $i | sed "s#KERNEL_IMAGE#${KERNEL_IMAGE}#g" | sed "s/VERSION_TAG/${VERSION_TAG}/g" > ${outfile}
     linuxkit build -dir cache --docker ${outfile}
     runfile=${outfile%%.yml}
-    echo "${KERNEL_IMAGE} ready for test, run:"
-    echo "  linuxkit run qemu -mem 2048 ${runfile}"
-    echo " or"
-    echo "  linuxkit run hyperkit -mem 2048 ${runfile}"
-    echo
+    outlines="${outlines}\n  linuxkit run ${hypervisor} -mem 2048 ${runfile}"
   done
 done < versions
+
+echo $outlines
 
 
